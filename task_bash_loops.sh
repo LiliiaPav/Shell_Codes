11fc146d974681865f81863be5ef772cee0dff3c
Liliia Pav <LiliiaPav@gmail.com>
2016-09-15 20:36:09 -0700

    loop with dates with step

--- task_bash_loops.sh
+++ task_bash_loops.sh
@@ -1,2 +1,21 @@
 #! /bin/bush
+echo Enter a step
+read step
 
+data_start=$(date -d "2016-08-01" +"%Y-%m-%d")
+data_end=$(date -d "2016-08-31" +"%Y-%m-%d")
+newd=$(date -d "$data_start - 1 day" +"%F")
+
+while [ $step ]
+do
+	date1=$(date -d "$newd + 1 day" +"%F")
+	date2=$(date -d "$newd + $step day" +"%F")
+	newd=$date2
+	if [[ "$date2" > "$data_end" ]] || [[ "$date1" > "$data_end" ]]
+	then 
+		echo $date1 - $data_end
+		break
+	fi
+	echo $date1 - $date2
+
+done 
